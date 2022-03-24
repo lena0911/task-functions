ba579d94f041e6da04b99476da8ec01ae0138ecc
lena0911 <lenagolovkina09@gmail.com>
2022-03-24 20:15:03 +0300

    Задание по функциям

--- src/Functions.js
+++ src/Functions.js
@@ -1,12 +1,18 @@
 /*
 Напишите коллбек для array.filter, который отбирает все числа больше num
  */
-export function higherThan(num) {}
+export function higherThan(num) {
+    let result = (a) => a > num;
+    return result;
+}
 
 /*
 Напишите коллбек для array.filter, который отбирает из массива все строки, включающие в себя substr. Регистр важен.
  */
-export function hasSubstring(substr) {}
+export function hasSubstring(substr) {
+    let result = (a) => a.includes(substr);
+    return result;
+}
 
 /*
 Напишите функцию `multiply(num)`, которая работает вот так:
@@ -14,4 +20,7 @@ export function hasSubstring(substr) {}
 console.log(multiply(5)(10));
 // 50
  */
-export function multiply(num) {}
+export function multiply(num) {
+    let result = (a) => a * num;
+    return result;
+}
